6c2749faab91c039b07b9c22a3c43fbf3b79d1ba
Kevin <kevinayers@macbook-pro.lan>
2020-12-22 18:31:32 -0800

    added styles and fetching users from server

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import CardList from './CardList';
 import SearchBox from './SearchBox';
-import { robots } from './robots'; 
+import './App.css';
 
 
 
@@ -9,10 +9,20 @@ class App extends Component {
     constructor(){
         super()
         this.state = {
-            robots: robots,
+            robots: [],
             searchfield: ''
 
         }
+    }
+
+    componentDidMount() {
+        fetch('https://jsonplaceholder.typicode.com/users')
+        .then(response=> {
+            return response.json();
+        })
+        .then(users => {
+            this.setState({ robots: users})
+        });
     }
 
     onSearchChange = (event) => {
@@ -26,7 +36,7 @@ class App extends Component {
 
         return (
             <div className='tc'>
-                <h1>RoboFriends</h1>
+                <h1 className='f1' >RoboFriends</h1>
                 <SearchBox searchChange={this.onSearchChange}/>
                 <CardList robots={filterdRobots}/>
             </div>
